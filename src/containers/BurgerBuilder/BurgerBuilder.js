435573b04bd105125eb8cb2ca66f766690c5feb4
Kevin Pulley <skippy1025@gmail.com>
2021-04-05 20:35:37 -0500

    added orders page and order component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,30 +89,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    //alert('You Continue');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Kevin Pulley",
-    //     address: {
-    //       street: "1088 Williamson Rd",
-    //       zipCode: "37072",
-    //       country: "North America",
-    //     },
-    //     email: "skippy1025@gmail.com",
-    //   },
-    //   deliveryMethod: "fastest",
-    // };
-    // axios
-    //   .post("/orders.json", order)
-    //   .then((response) => {
-    //     this.setState({ loading: false, purchasing: false});
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(
@@ -121,7 +98,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i])
       );
     }
-
+queryParams.push('price=' + this.state.totalPrice)
     const queryString = queryParams.join('&');
     
 
